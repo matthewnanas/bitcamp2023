ff8559d7ac1c83a07dea5a362aac0378264aa59b
matthewnanas <spazeplayz@gmail.com>
2023-04-08 00:40:55 -0400

    wip

---
+++ webapp/src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Navbar() {
+    return (
+        <div>
+            <h1>This is the Navbar component</h1>
+        </div>
+    )
+}
---
+++ webapp/src/Components/Register/Register.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Register() {
+    return (
+        <div>
+            <h1>This is the Register modal</h1>
+        </div>
+    )
+}
