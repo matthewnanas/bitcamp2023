d029c00201bd468fb21026208d697225d7a828ed
Rohit Sharma <kprsharma21@gmail.com>
2023-04-08 02:55:11 -0400

    remove numbers, creating accounts, hashing

---
+++ server/db/schemas/accounts.js
@@ -0,0 +1,12 @@
+const { model, Schema } = require('mongoose');
+
+const accounts = model('accounts', new Schema({
+    email: String,
+    password: String,
+    numbers: Array,
+    created: Date
+}));
+
+
+
+module.exports = accounts;
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
+const crypto = require('crypto');
 
 var jsonParser = bodyParser.json()
 
@@ -10,6 +11,7 @@ const port = 3001;
 mongoose.connect("mongodb://localhost:27017/admin");
 
 const numbers = require("./db/schemas/numbers.js");
+const accounts = require("./db/schemas/accounts.js");
 // const incidents = "./db/schemas/incidents.js";
 
 
@@ -19,6 +21,14 @@ async function exists(db, query) {
 }
 
 
+async function removePhone(number) {
+    number = await numbers.findOneAndRemove({number: numberToRemove});
+    if(number) {
+        return {success: true, message: "Number removed"};
+    } else {
+        return {success: false, message: "Number not removed"};
+    }
+}
 
 app.post('/addPhone', jsonParser, async(req, res) => {
     // TODO: Add phone to database
@@ -40,24 +50,63 @@ app.post('/addPhone', jsonParser, async(req, res) => {
 app.post('/removePhone', jsonParser, async(req, res) => {
     // TODO: Remove phone from database
     let numberToRemove = req.body.number;
-    let number = await numbers.findOneAndRemove({number: numberToRemove});
-    if(number) {
-        res.json({success: true, message: "Number removed"});
-    } else {
-        res.json({success: false, message: "Number not removed"});
-    }
+    removePhone(numberToRemove);
 });
 
 app.post('/incomingSms', (req, res) => {
     // Check if message is "stop," check if number in db
     if(req.body.lower() == "stop") {
-        // remove from DB, reply
+        removed = removePhone(req.body.number);
+        if(removed.success) {
+            res.json({success: true, message: "Number removed"});
+        } else {
+            res.json({success: false, message: "Number not removed"});
+        }
     }
-    
-
 
 });
 
+app.post('/createAccount', jsonParser, async(req, res) => {
+    let email = req.body.email;
+    let account = await accounts.findOne({email: email});
+    if(!account) {
+        let hash = crypto.createHash('sha256').update(req.body.password).digest('hex');
+        let accountCreated = await accounts.create({
+            email: email,
+            password: hash,
+            numbers: [],
+            created: Date.now()
+        });
+        if(accountCreated) {
+            res.json({status: true, message: "Account created successfully"});
+        } else {
+            res.json({status: false, message: "Account not created"});
+        }
+        
+    }
+    res.json({status: false, message: "Account exists"});
+
+})
+
+app.post('/login', jsonParser, async(req, res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+    let account = await accounts.findOne({email: email});
+    if(account) {
+        let hash = crypto.createHash('sha256').update(password).digest('hex');
+        if(hash==account.password) {
+            res.json({status: true, message: "Logged in"});
+            // login
+        } else {
+            res.json({status: false, message: "Account doesn't exist"});
+            //
+        }
+    } else {
+        res.json({status: false, message: "Account doesn't exist"});
+
+        console.log(account)
+    }
+});
 
 
 
